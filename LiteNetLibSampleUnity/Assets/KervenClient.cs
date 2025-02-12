5c4ab1db14a5f504498eb37c2bc6851d55ffd638
hankangwen <13538898378@163.com>
2025-02-12 22:08:13 +0800

    完成Server和Client

---
+++ LiteNetLibSampleUnity/Assets/KervenClient.cs
@@ -0,0 +1,110 @@
+using System.Net;
+using System.Net.Sockets;
+using LiteNetLib;
+using UnityEngine;
+
+public class KervenClient : MonoBehaviour, INetEventListener
+{
+    private NetManager _netClient;
+
+    [SerializeField] private GameObject _clientBall;
+    [SerializeField] private GameObject _clientBallInterpolated;
+
+    private float _newBallPosX;
+    private float _oldBallPosX;
+    private float _lerpTime;
+
+    private void Start()
+    {
+        _netClient = new NetManager(this);
+        _netClient.UnconnectedMessagesEnabled = true;
+        _netClient.UpdateTime = 15;
+        _netClient.Start();
+
+        // _netClient.Connect("localhost" /* host ip or name */, 9050 /* port */, "sample_app" /* text key or NetDataWriter */);
+    }
+
+    private void Update()
+    {
+        _netClient.PollEvents();
+
+        var peer = _netClient.FirstPeer;
+        if (peer != null && peer.ConnectionState == ConnectionState.Connected)
+        {
+            //Fixed delta set to 0.05
+            var pos = _clientBallInterpolated.transform.position;
+            pos.x = Mathf.Lerp(_oldBallPosX, _newBallPosX, _lerpTime);
+            _clientBallInterpolated.transform.position = pos;
+
+            //Basic lerp
+            _lerpTime += Time.deltaTime / Time.fixedDeltaTime;
+        }
+        else
+        {
+            _netClient.SendBroadcast(new byte[] {1}, 9050);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (_netClient != null)
+            _netClient.Stop();
+    }
+
+    void INetEventListener.OnPeerConnected(NetPeer peer)
+    {
+        Debug.Log("[CLIENT] We connected to " + peer);
+    }
+
+    void INetEventListener.OnNetworkError(IPEndPoint endPoint, SocketError socketErrorCode)
+    {
+        Debug.Log("[CLIENT] We received error " + socketErrorCode);
+    }
+
+    void INetEventListener.OnNetworkReceive(NetPeer peer, NetPacketReader dataReader, byte channelNumber, DeliveryMethod deliveryMethod)
+    {
+        if (deliveryMethod == DeliveryMethod.ReliableOrdered)
+        {
+            Debug.Log($"We got: {dataReader.GetString(100)}");
+            dataReader.Recycle();
+            return;
+        }
+
+        _newBallPosX = dataReader.GetFloat();
+
+        var pos = _clientBall.transform.position;
+
+        _oldBallPosX = pos.x;
+        pos.x = _newBallPosX;
+
+        _clientBall.transform.position = pos;
+
+        _lerpTime = 0f;
+
+        dataReader.Recycle();
+    }
+
+    void INetEventListener.OnNetworkReceiveUnconnected(IPEndPoint remoteEndPoint, NetPacketReader reader, UnconnectedMessageType messageType)
+    {
+        if (messageType == UnconnectedMessageType.BasicMessage && _netClient.ConnectedPeersCount == 0 && reader.GetInt() == 1)
+        {
+            Debug.Log("[CLIENT] Received discovery response. Connecting to: " + remoteEndPoint);
+            _netClient.Connect(remoteEndPoint, "sample_app");
+        }
+    }
+
+    void INetEventListener.OnNetworkLatencyUpdate(NetPeer peer, int latency)
+    {
+
+    }
+
+    void INetEventListener.OnConnectionRequest(ConnectionRequest request)
+    {
+
+    }
+
+    void INetEventListener.OnPeerDisconnected(NetPeer peer, DisconnectInfo disconnectInfo)
+    {
+        Debug.Log("[CLIENT] We disconnected because " + disconnectInfo.Reason);
+    }
+}
---
+++ LiteNetLibSampleUnityServer/Assets/KervenServer.cs
@@ -0,0 +1,105 @@
+using System;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+using LiteNetLib;
+using LiteNetLib.Utils;
+
+public class KervenServer : MonoBehaviour, INetEventListener, INetLogger
+{
+    private NetManager _netServer;
+    private NetPeer _ourPeer;
+    private NetDataWriter _dataWriter;
+    private const int MaxClientConnections = 10;
+
+    [SerializeField] private GameObject _serverBall;
+
+    private void Start()
+    {
+        NetDebug.Logger = this;
+        _dataWriter = new NetDataWriter();
+        _netServer = new NetManager(this);
+        _netServer.Start(9050);
+        _netServer.BroadcastReceiveEnabled = true;
+        _netServer.UpdateTime = 15;
+    }
+
+    private void Update()
+    {
+        _netServer.PollEvents();
+    }
+
+    private void FixedUpdate()
+    {
+        if (_ourPeer != null)
+        {
+            _serverBall.transform.Translate(1f * Time.fixedDeltaTime, 0f, 0f);
+            _dataWriter.Reset();
+            _dataWriter.Put(_serverBall.transform.position.x);
+            _ourPeer.Send(_dataWriter, DeliveryMethod.Sequenced);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        NetDebug.Logger = null;
+        if (_netServer != null)
+            _netServer.Stop();
+    }
+
+
+    void INetEventListener.OnPeerConnected(NetPeer peer)
+    {
+        Debug.Log("[SERVER] We have new peer " + peer);
+        _ourPeer = peer;
+
+        NetDataWriter writer = new NetDataWriter();         // Create writer class
+        writer.Put("Hello client!");                        // Put some string
+        peer.Send(writer, DeliveryMethod.ReliableOrdered);  // Send with reliability
+    }
+
+    void INetEventListener.OnNetworkError(IPEndPoint endPoint, SocketError socketErrorCode)
+    {
+        Debug.Log("[SERVER] error " + socketErrorCode);
+    }
+
+    void INetEventListener.OnNetworkReceiveUnconnected(IPEndPoint remoteEndPoint, NetPacketReader reader,
+        UnconnectedMessageType messageType)
+    {
+        if (messageType == UnconnectedMessageType.Broadcast)
+        {
+            Debug.Log("[SERVER] Received discovery request. Send discovery response");
+            NetDataWriter resp = new NetDataWriter();
+            resp.Put(1);
+            _netServer.SendUnconnectedMessage(resp, remoteEndPoint);
+        }
+    }
+
+    void INetEventListener.OnNetworkLatencyUpdate(NetPeer peer, int latency)
+    {
+    }
+
+    void INetEventListener.OnConnectionRequest(ConnectionRequest request)
+    {
+        if(_netServer.ConnectedPeersCount < MaxClientConnections)
+            request.AcceptIfKey("sample_app");
+        else
+            request.Reject();
+    }
+
+    void INetEventListener.OnPeerDisconnected(NetPeer peer, DisconnectInfo disconnectInfo)
+    {
+        Debug.Log("[SERVER] peer disconnected " + peer + ", info: " + disconnectInfo.Reason);
+        if (peer == _ourPeer)
+            _ourPeer = null;
+    }
+
+    void INetEventListener.OnNetworkReceive(NetPeer peer, NetPacketReader reader, byte channelNumber, DeliveryMethod deliveryMethod)
+    {
+    }
+
+    void INetLogger.WriteNet(NetLogLevel level, string str, params object[] args)
+    {
+        Debug.LogFormat(str, args);
+    }
+}

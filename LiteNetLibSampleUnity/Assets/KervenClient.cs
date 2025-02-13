741753ec8ce8f38c62b0d57335fc9cea8c027017
hankangwen <13538898378@163.com>
2025-02-13 10:51:16 +0800

    1.3.0->1.3.3

--- LiteNetLibSampleUnity/Assets/KervenClient.cs
+++ LiteNetLibSampleUnity/Assets/KervenClient.cs
@@ -22,11 +22,11 @@ public class KervenClient : MonoBehaviour, INetEventListener
         _netClient.Start();
 
         // remote aliyun
-        _netClient.Connect("8.138.136.204", 80, "sample_app");
+        // _netClient.Connect("8.138.136.204", 80, "sample_app");
         // local
         // _netClient.Connect("10.11.176.107", 80, "sample_app");
         // _netClient.Connect("fe80::2ef9:6833:20fa:d34e%4", 80, "sample_app");
-        // _netClient.Connect("127.0.0.1", 80, "sample_app");
+        _netClient.Connect("127.0.0.1", 80, "sample_app");
     }
 
     private void Update()
